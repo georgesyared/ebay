fac1639065500318eb7a1a123d702a72bfeec541
Yared <georges_yared99@hotmail.com>
2022-10-28 22:16:50 +0300

    create home page , implement web3 authentication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-
+import { ThirdwebProvider} from "@thirdweb-dev/react";
+import  network  from "../utils/network"
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+
+  //Etherium Goerli
+  <ThirdwebProvider desiredChainId={network}>
+    <Component {...pageProps} />
+  </ThirdwebProvider>
+  
+  )
 }
 
 export default MyApp
