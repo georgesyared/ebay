7c2f9e69ad3679b598a32b54bb93d958ed35141e
Yared <georges_yared99@hotmail.com>
2022-10-29 01:23:40 +0300

    marketplace/collection

--- components/Header.tsx
+++ components/Header.tsx
@@ -76,12 +76,14 @@ function Header({}: Props) {
           />
         </div>
 
-        <button className="hidden sm:inline bg-blue-600 text-white px-5 md:px-10 py-2 border-2 border-blue-600">
+        <button className="hidden sm:inline bg-blue-600
+         text-white px-5 md:px-10 py-2 border-2 border-blue-600">
           Search
         </button>
 
         <Link href="/create">
-          <button className="border-2 border-blue-600 px-5 md:px-10 py-2 text-blue-600 hover:bg-blue-600/50 hover:text-white">
+          <button className="border-2 border-blue-600
+           px-5 md:px-10 py-2 text-blue-600 hover:bg-blue-600/50 hover:text-white">
             List Item
           </button>
         </Link>
